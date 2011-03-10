f32913c8372b251ae502a3cfa73062a43bca55e1
zdk <nx2zdk@gmail.com>
2011-03-10 12:27:02 +0700

    Add POD

--- lib/Catalyst/Plugin/Session/State/Auth.pm
+++ lib/Catalyst/Plugin/Session/State/Auth.pm
@@ -1,11 +1,11 @@
 package Catalyst::Plugin::Session::State::Auth;
 use Moose;
-use HTTP::Headers::Util qw(split_header_words);
 use MRO::Compat;
+use HTTP::Headers::Util qw(split_header_words);
 
 use namespace::clean -except => 'meta';
 
-our $VERSION = '0.001';
+our $VERSION = '0.0001';
 
 extends 'Catalyst::Plugin::Session::State';
 with 'MooseX::Emulate::Class::Accessor::Fast';
@@ -17,18 +17,30 @@ sub get_session_id {
     return $c->_sessionid_from_auth_signature || $c->maybe::next::method(@args);
 }
 
+sub delete_session_id {
+    my ( $c, @args ) = @_;
+    $c->_sessionid_from_auth_signature(undef);
+    $c->maybe::next::method(@args);
+}
+
 sub finalize {
     my $c = shift;
     return $c->maybe::next::method(@_);
 }
 
-sub prepare_action {
+sub prepare_path {
     my $c = shift;
-    my @values = split_header_words( $c->request->header('authorization') );
-    $sid       = $values[-1][-1];
-    $c->_sessionid_from_uri( $sid );
-    $c->log->debug(qq/Found sessionid "$sid" in Authorization: signature/) if $c->debug;
-    return $c->maybe::next::method(@_);
+
+    $c->maybe::next::method(@_);
+
+    if ( $c->request->header('authorization') ){
+        my @values     = split_header_words( $c->request->header('authorization') );
+        my $signature  = $values[-1][-1];
+        $c->_sessionid_from_auth_signature($signature);
+        $c->_tried_loading_session_id(0);
+        $c->log->debug(qq/Found sessionid "$signature" in MAC authentication/)
+          if $c->debug;
+    }
 }
 
 __PACKAGE__
@@ -39,21 +51,45 @@ __END__
 
 =head1 NAME
 
-Catalyst::Plugin::Session::State::Auth - 
+Catalyst::Plugin::Session::State::Auth -
+Use signature in HTTP MAC authentication scheme to pass the session id between requests
+
+=head1 SYNOPSIS
+    In MyApp.pm
+    use Catalyst qw/
+      Session
+      Session::State::Auth
+      Session::Store::Foo
+      /;
 
 =head1 DESCRIPTION
+
+In your HTTP request
+Authorization: MAC token="h480djs93hd8",
+                   timestamp="137131200",
+                   nonce="dj83hs9s",
+                   signature="kDZvddkndxvhGRXZhvuDjEWhGeE="
+
+Use the signature to pass your sessionid
 
 =head1 METHODS
 
 =head1 BUGS
 
+=head1 SEE ALSO
+
+L<Catalyst>, L<Catalyst::Plugin::Session>,L<Catalyst::Plugin::Session::State::URI>.
+
 =head1 AUTHOR
+
+Warachet Samtalee (zdk)
+
+This module is derived from L<CCatalyst::Plugin::Session::State::URI> code.
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2009 the above author(s).
+Copyright 2011 the above author(s).
 
 This sofware is free software, and is licensed under the same terms as perl itself.
 
 =cut
-
